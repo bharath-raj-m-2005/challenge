cc996a2b6ecb762830d49cc3474c735eb2a2c560
bharathrajm-2005 <mbharathrajcw@gmail.com.com>
2026-02-12 21:22:23 +0530

    your message

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['localhost'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
+    domains: [],
   },
 };
 
